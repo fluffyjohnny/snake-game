38a35d96844079f411ea0e16d629e8b8abf9aaf4
John Chia <john.chia0106@gmail.com>
2022-08-17 16:36:36 -0700

    create function to set starting location of the nake linkedlist value

--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -26,6 +26,19 @@ const Direction = {
 
 const BOARD_SIZE = 10;
 
+const getStartingSnakeLLValue = board => {
+  const rowSize = board.length; 
+  const colSize = board[0].length;
+  const startingRow = Math.round(rowSize / 3);
+  const startingCol = Math.round(colSize / 3);
+  const startingCell = board[startingRow][startingCol];
+  return {
+    row: startingRow,
+    col: startingCol,
+    cell: startingCell,
+  }
+}
+
 const Board = () => {
   const [board, setBoard] = useState(createBoard(BOARD_SIZE));
   const [snakeCells, setSnakeCells] = useState(new Set([44]));
