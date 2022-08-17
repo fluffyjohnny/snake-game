60459bd05380f326600d883a43dbe5ffdd5c2fe7
John Chia <john.chia0106@gmail.com>
2022-08-16 23:04:18 -0700

    add ternary statuement for cell classname

--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -15,7 +15,7 @@ const Board = () => {
       {board.map((row, rowIdx) => (
         <div key={rowIdx} className="row">
           {row.map((cell, cellIdx) => (
-            <div key={cellIdx} className="cell"></div>
+            <div key={cellIdx} className={`cell ${true ? 'snake-cell' : ''}`}></div>
           ))}
         </div>
       ))}
