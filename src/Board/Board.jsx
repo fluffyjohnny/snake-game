1dee53e412c22fc69f6b3ae96f0ff16cec2926aa
John Chia <john.chia0106@gmail.com>
2022-08-17 16:02:52 -0700

    change board generation to a function instead of array constructor, and added linkedlist classes

--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -2,20 +2,48 @@ import { useState } from "react";
 
 import "./Board.css";
 
+class LinkedListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor(value) {
+    const node = new LinkedListNode(value);
+    this.head = node;
+    this.tail = node;
+  }
+}
+
 const BOARD_SIZE = 10;
 
+const createBoard = (boardSize) => {
+  let counter = 1;
+  const board = [];
+  for (let row = 0; row < boardSize; row++) {
+    const currentRow = [];
+    for (let col = 0; col < boardSize; col++) {
+      currentRow.push(counter++);
+    }
+    board.push(currentRow);
+  }
+  return board;
+};
+
 const Board = () => {
-  const [board, setBoard] = useState(
-    // creates a new empty 2-D array
-    new Array(BOARD_SIZE).fill(0).map((row) => new Array(BOARD_SIZE).fill(0))
-  );
+  const [board, setBoard] = useState(createBoard(BOARD_SIZE));
 
   return (
     <div className="board">
       {board.map((row, rowIdx) => (
         <div key={rowIdx} className="row">
           {row.map((cell, cellIdx) => (
-            <div key={cellIdx} className={`cell ${true ? 'snake-cell' : ''}`}></div>
+            <div
+              key={cellIdx}
+              className={`cell ${true ? "snake-cell" : ""}`}
+            >{cell}</div>
           ))}
         </div>
       ))}
