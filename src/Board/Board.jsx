50aeef65dd98320bae49b2fc7aa0ce00ca67ada7
John Chia <john.chia0106@gmail.com>
2022-08-16 22:55:37 -0700

    generate empty board with changable board size

--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -1,4 +1,26 @@
 import { useState } from "react";
 
-import './Board.css';
+import "./Board.css";
 
+const BOARD_SIZE = 10;
+
+const Board = () => {
+  const [board, setBoard] = useState(
+    // creates a new empty 2-D array
+    new Array(BOARD_SIZE).fill(0).map((row) => new Array(BOARD_SIZE).fill(0))
+  );
+
+  return (
+    <div className="board">
+      {board.map((row, rowIdx) => (
+        <div key={rowIdx} className="row">
+          {row.map((cell, cellIdx) => (
+            <div key={cellIdx} className="cell"></div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Board;
