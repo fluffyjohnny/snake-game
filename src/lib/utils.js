40fd2f04e86a57655cbddc81f88dbb15dbb80ee4
John Chia <john.chia0106@gmail.com>
2022-08-17 21:39:40 -0700

    create a set delay function since setInterval function does not work in useEffect

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,26 @@
+import { useEffect, useRef } from "react";
 
 // create function to generate point where apples spawns
 const randomIntFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const useInterval = (callback, delay) => {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    const tick = () => {
+      savedCallback.current();
+    } 
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay])
 }
+
+export { randomIntFromInterval };
